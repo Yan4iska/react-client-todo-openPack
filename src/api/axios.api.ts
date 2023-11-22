a667cae83fd7154d80f2db17ac0223c612a9f9a8
yan4iska <matveev.git@gmail.com>
2023-11-22 18:27:11 +1000

    registration
    created registration on web app(client - axios - server)

---
+++ src/api/axios.api.ts
@@ -0,0 +1,10 @@
+import axios from 'axios'
+import { getTokenFromStorage } from '../helper/localstorage.helper'
+
+export const instance = axios.create({
+    baseURL: 'http://localhost:3000/api',
+    headers:{
+        Authorization: `Bearer` + getTokenFromStorage() || '',
+        
+    },
+})
--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,8 +1,68 @@
-import { FC } from "react"
+import { FC, useState } from "react"
+import { AuthService } from "../services/auth.service"
+import { toast } from "react-toastify"
 
 const Auth: FC = () => {
+  const [email, setEmail] = useState('')
+  const [isLogin, setIsLogin] = useState<boolean>(false)
+  const [password, setPassword] = useState('')
+
+  const loginHandler = async(e: React.FormEvent<HTMLFormElement>) => {
+    try{
+      
+      
+    } catch (err: any){
+      const error = err.responce ?. data.message
+      toast.error(error.toString())
+    }
+  }
+
+  const registrationHandler = async(e: React.FormEvent<HTMLFormElement>) => {
+    try{
+      e.preventDefault()
+      const data = await AuthService.registration({ email, password })
+      if(data) {
+        toast.success('Account has been created!')
+        setIsLogin(!isLogin)
+//
+      }
+    }catch (err: any){
+      const error = err.responce ?. data.message
+      toast.error(error.toString())
+    }
+  }
+
   return (
-    <div>Auth</div>
+    <div className="auth">
+      <h1 className="auth__h1">
+        {
+          isLogin ? 'Login' : 'Registration'
+        }
+      </h1>
+      <form 
+      onSubmit={isLogin ? loginHandler : registrationHandler}
+      action="" className="auth__form">
+      <input type="text" className="input" placeholder="Email" onChange={(e)=> setEmail(e.target.value)} />
+      <input type="password" className="input" placeholder="Password" onChange={(e)=> setPassword(e.target.value)} />
+        <button className="btn btn-green">Submit</button>
+      </form>
+
+      <div className="registration">
+        {isLogin ? (
+        <button
+          onClick={()=>setIsLogin(!isLogin)}
+          className="btnLinks">
+          You don't have an account?
+        </button>
+        ) : (
+          <button 
+          onClick={()=>setIsLogin(!isLogin)}
+          className="btnLinks">
+          Already have an account?
+        </button>
+        )}
+      </div>
+    </div>
   )
 }
 
